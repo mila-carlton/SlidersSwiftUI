e8b9b73ba9bbd8241db9b4171ec34fd5f1b6b13d
mila-carlton <mila.carlton@mail.ru>
2023-12-16 20:08:25 +0400

    Initial Commit

---
+++ SlidersSwiftUI/SlidersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidersSwiftUIApp.swift
+//  SlidersSwiftUI
+//
+//  Created by PASGON TEXTILE on 16.12.23.
+//
+
+import SwiftUI
+
+@main
+struct SlidersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
